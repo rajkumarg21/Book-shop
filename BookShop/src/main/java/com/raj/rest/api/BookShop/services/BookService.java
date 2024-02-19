cdb0dd3bb91ef403fab44dbfdec1840f2977df29
Rajkumar saad <58967581+rajkumarg21@users.noreply.github.com>
2024-02-19 14:28:11 +0530

    Add files via upload

---
+++ BookShop/src/main/java/com/raj/rest/api/BookShop/services/BookService.java
@@ -0,0 +1,10 @@
+package com.raj.rest.api.BookShop.services;
+
+import java.util.List;
+
+import com.raj.rest.api.BookShop.entities.Book;
+
+public interface BookService {
+	public List<Book> getBooks();
+
+}
---
+++ BookShop/src/test/java/com/raj/rest/api/BookShop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.rest.api.BookShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
